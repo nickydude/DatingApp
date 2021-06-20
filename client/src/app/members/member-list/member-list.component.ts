06b1ffe8991f32f9226c17c403aaf3b8306c1d51
Nicholas Siew <nicsiew@gmail.com>
2021-06-20 21:55:14 +0800

    added persist member edit functionality and loading screen and also caching members data

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from '../../_models/member';
 import { MemberService } from '../../_services/member.service';
 
@@ -9,18 +10,14 @@ import { MemberService } from '../../_services/member.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members: Member[];
+  members$: Observable<Member[]>;
 
   constructor(private memberService: MemberService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
 
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    });
-  }
+ 
 
 }
